672a155a33dbe3a8716fd5c5c2cfe01303bbdc9d
Marc Frei <marc.frei@gmail.com>
2021-09-01 11:40:03 +0200

    Add minimal client/server test programs

---
+++ experiments/zero/c.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+
+	"github.com/scionproto/scion/go/lib/addr"
+	"github.com/scionproto/scion/go/lib/sciond"
+	"github.com/scionproto/scion/go/lib/snet"
+	"github.com/scionproto/scion/go/lib/sock/reliable"
+	"github.com/scionproto/scion/go/lib/sock/reliable/reconnect"
+	"github.com/scionproto/scion/go/lib/topology/underlay"
+)
+
+func sendHello(sciondAddr, dispatcherSocket string, localAddr snet.UDPAddr, remoteAddr snet.UDPAddr) {
+	var err error
+	ctx := context.Background()
+
+	sdc, err := sciond.NewService(sciondAddr).Connect(ctx)
+	if err != nil {
+		log.Fatal("Failed to create SCION connector:", err)
+	}
+	pds := &snet.DefaultPacketDispatcherService{
+		Dispatcher: reconnect.NewDispatcherService(
+			reliable.NewDispatcher(dispatcherSocket)),
+		SCMPHandler: snet.DefaultSCMPHandler{
+			RevocationHandler: sciond.RevHandler{Connector: sdc},
+		},
+	}
+
+	ps, err := sdc.Paths(ctx, remoteAddr.IA, localAddr.IA, sciond.PathReqFlags{Refresh: true})
+	if err != nil {
+		log.Fatal("Failed to lookup core paths: %v:", err)
+	}
+
+	log.Printf("Available paths to %v:\n", remoteAddr.IA)
+	for _, p := range ps {
+		log.Printf("\t%v\n", p)
+	}
+
+	sp := ps[0]
+	log.Printf("Selected path to %v: %v\n", remoteAddr.IA, sp)
+
+	localAddr.Host.Port = 0
+	conn, localPort, err := pds.Register(ctx, localAddr.IA, localAddr.Host, addr.SvcNone)
+	if err != nil {
+		log.Fatal("Failed to register client socket:", err)
+	}
+
+	log.Printf("Sending in %v on %v:%d - %v\n", localAddr.IA, localAddr.Host.IP, localPort, addr.SvcNone)
+
+	pkt := &snet.Packet{
+		PacketInfo: snet.PacketInfo{
+			Source: snet.SCIONAddress{
+				IA: localAddr.IA,
+				Host: addr.HostFromIP(localAddr.Host.IP),
+			},
+			Destination: snet.SCIONAddress{
+				IA: remoteAddr.IA,
+				Host: addr.HostFromIP(remoteAddr.Host.IP),
+			},
+			Path: sp.Path(),
+			Payload: snet.UDPPayload{
+				SrcPort: localPort,
+				DstPort: uint16(remoteAddr.Host.Port),
+				Payload: []byte("Hello, world!"),
+			},
+		},
+	}
+
+	nextHop := sp.UnderlayNextHop()
+	if nextHop == nil && remoteAddr.IA.Equal(localAddr.IA) {
+		nextHop = &net.UDPAddr{
+			IP: remoteAddr.Host.IP,
+			Port: underlay.EndhostPort,
+			Zone: remoteAddr.Host.Zone,
+		}
+	}
+
+	err = conn.WriteTo(pkt, nextHop)
+	if err != nil {
+		log.Printf("Failed to write packet: %v\n", err)
+		return
+	}
+
+	var lastHop net.UDPAddr
+	err = conn.ReadFrom(pkt, &lastHop)
+	if err != nil {
+		log.Printf("Failed to read packet: %v\n", err)
+		return
+	}
+	pld, ok := pkt.Payload.(snet.UDPPayload)
+	if !ok {
+		log.Printf("Failed to read packet payload\n")
+		return
+	}
+	data := string(pld.Payload);
+	log.Printf("Received payload: \"%v\"\n", data)
+}
+
+func main() {
+	var sciondAddr string
+	var dispatcherSocket string
+	var localAddr snet.UDPAddr
+	var remoteAddr snet.UDPAddr
+	flag.StringVar(&sciondAddr, "sciond", "", "sciond address")
+	flag.StringVar(&dispatcherSocket, "dispatcher", "", "dispatcher socket")
+	flag.Var(&localAddr, "local", "Local address")
+	flag.Var(&remoteAddr, "remote", "Remote address")
+	flag.Parse()
+
+	sendHello(sciondAddr, dispatcherSocket, localAddr, remoteAddr)
+}
---
+++ experiments/zero/go.mod
@@ -0,0 +1,39 @@
+module example.com/scion-time
+
+go 1.17
+
+require github.com/scionproto/scion v0.0.0-00010101000000-000000000000
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/dchest/cmac v0.0.0-20150527144652-62ff55a1048c // indirect
+	github.com/go-kit/kit v0.10.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/gopacket v1.1.16-0.20190123011826-102d5ca2098c // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.1-0.20190118093823-f849b5445de4 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
+	github.com/grpc-ecosystem/grpc-opentracing v0.0.0-20180507213350-8e809c8a8645 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pelletier/go-toml v1.9.3 // indirect
+	github.com/prometheus/client_golang v1.6.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.9.1 // indirect
+	github.com/prometheus/procfs v0.0.11 // indirect
+	github.com/uber/jaeger-client-go v2.20.1+incompatible // indirect
+	github.com/uber/jaeger-lib v2.0.0+incompatible // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.17.0 // indirect
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
+	google.golang.org/grpc v1.38.1 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
+
+replace github.com/scionproto/scion => github.com/netsec-ethz/scion v0.0.0-20210825190202-fc081bebc932
