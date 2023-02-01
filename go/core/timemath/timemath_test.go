502f9bb198610ef5d8c80833449119e05d6e9a83
Marc Frei <marc.frei@gmail.com>
2023-02-01 16:33:09 +0100

    Fix overflow errors in timemath

---
+++ go/core/timemath/timemath_test.go
@@ -0,0 +1,32 @@
+package timemath_test
+
+import (
+	"math"
+	"testing"
+	"time"
+
+	"example.com/scion-time/go/core/timemath"
+)
+
+func TestMedian(t *testing.T) {
+	ds := []time.Duration{time.Duration(math.MaxInt64), time.Duration(math.MaxInt64 - 1)}
+	m := ds[1]
+	x := timemath.Median(ds)
+	if x != m {
+		t.Errorf("Median(%v) == %d; want %d", ds, x, m)
+	}
+}
+
+func TestFaultTolerantMidpoint(t *testing.T) {
+	ds := []time.Duration{
+		time.Duration(math.MaxInt64),
+		time.Duration(math.MaxInt64 - 1),
+		time.Duration(math.MaxInt64 - 2),
+		time.Duration(math.MaxInt64 - 3),
+	}
+	m := ds[2]
+	x := timemath.FaultTolerantMidpoint(ds)
+	if x != m {
+		t.Errorf("FaultTolerantMidpoint(%v) == %d; want %d", ds, x, m)
+	}
+}
