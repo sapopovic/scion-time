0b2c7ae945b3c3821d5991484424049fdb6b52ee
Marc Frei <marc.frei@gmail.com>
2023-02-03 15:51:17 +0100

    Remove epoch flag from DRKey SPI

--- go/net/scion/drkey.go
+++ go/net/scion/drkey.go
@@ -8,20 +8,17 @@ const (
 	DRKeyTypeHostHost          = 1
 	DRKeyDirectionSenderSide   = 0
 	DRKeyDirectionReceiverSide = 1
-	DRKeyEpochLater            = 0
 	DRKeyProtoIdTS             = drkey.SCMP
 
 	PacketAuthMetadataLen = 12
 	PacketAuthMACLen      = 16
 	PacketAuthOptDataLen  = PacketAuthMetadataLen + PacketAuthMACLen
 
-	PacketAuthClientSPI = uint32(DRKeyTypeHostHost)<<18 |
-		uint32(DRKeyDirectionReceiverSide)<<17 |
-		uint32(DRKeyEpochLater)<<16 |
+	PacketAuthClientSPI = uint32(DRKeyTypeHostHost)<<17 |
+		uint32(DRKeyDirectionReceiverSide)<<16 |
 		uint32(DRKeyProtoIdTS)
-	PacketAuthServerSPI = uint32(DRKeyTypeHostHost)<<18 |
-		uint32(DRKeyDirectionSenderSide)<<17 |
-		uint32(DRKeyEpochLater)<<16 |
+	PacketAuthServerSPI = uint32(DRKeyTypeHostHost)<<17 |
+		uint32(DRKeyDirectionSenderSide)<<16 |
 		uint32(DRKeyProtoIdTS)
 	PacketAuthAlgorithm = uint8(0) // AES-CMAC
 )
