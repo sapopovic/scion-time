be687de304b695d0a7ad79f4cbfa7d43dbd03695
Marc Frei <marc.frei@gmail.com>
2023-02-03 18:26:00 +0100

    Cache secret values in DRKey fetcher

--- go/drkeyutil/fetcher.go
+++ go/drkeyutil/fetcher.go
@@ -9,10 +9,19 @@ import (
 
 type Fetcher struct {
 	dc daemon.Connector
+	svs map[drkey.Protocol]drkey.SecretValue
 }
 
 func (f *Fetcher) FetchSecretValue(ctx context.Context, meta drkey.SecretValueMeta) (drkey.SecretValue, error) {
-	return FetchSecretValue(ctx, f.dc, meta)
+	var err error
+	sv, ok := f.svs[meta.ProtoId]
+	if !ok || !sv.Epoch.Contains(meta.Validity) {
+		sv, err = FetchSecretValue(ctx, f.dc, meta)
+		if err == nil {
+			f.svs[meta.ProtoId] = sv
+		}
+	}
+	return sv, err
 }
 
 func (f *Fetcher) FetchHostHostKey(ctx context.Context, meta drkey.HostHostMeta) (drkey.HostHostKey, error) {
@@ -20,5 +29,8 @@ func (f *Fetcher) FetchHostHostKey(ctx context.Context, meta drkey.HostHostMeta)
 }
 
 func NewFetcher(c daemon.Connector) *Fetcher {
-	return &Fetcher{dc: c}
+	return &Fetcher{
+		dc: c,
+		svs: make(map[drkey.Protocol]drkey.SecretValue),
+	}
 }
