58d78ffaf16a70071f90a2f7b4820cd660f1410a
Marc Frei <marc.frei@gmail.com>
2023-03-20 11:20:23 +0100

    Reorganize testnet directory

--- testnet/duo/scion-topo-gen.sh
+++ testnet/duo/scion-topo-gen.sh
@@ -6,7 +6,7 @@ cd ~/scion
 rm -rf gen*
 printf '#!/bin/bash\necho "0.0.0.0"' > tools/docker-ip
 sed -i "s/DEFAULT_NETWORK = \"127\.0\.0\.0\/8\"/DEFAULT_NETWORK = \"10\.0\.0\.0\/16\"/" tools/topology/net.py
-tools/topogen.py -c ~/scion-time/testnet/duo4.topo
+tools/topogen.py -c ~/scion-time/testnet/duo/duo.topo
 git checkout --quiet tools/topology/net.py
 git checkout --quiet tools/docker-ip
 
