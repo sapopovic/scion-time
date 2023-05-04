880d53e6cf5454f9b5822db1bb633e62ac96c8ec
Aaron Bojarski <eques.superbus@gmail.com>
2023-05-04 10:47:32 +0200

    correct chrony installation

--- testnet/aws/chrony.sh
+++ testnet/aws/chrony.sh
@@ -2,6 +2,7 @@ sudo apt-get install libgnutls28-dev
 sudo apt-get install gnutls-bin
 sudo apt-get install libtomcrypt-dev
 sudo apt-get install nettle-dev
+sudo apt-get install pkg-config
 curl -LO https://download.tuxfamily.org/chrony/chrony-4.3.tar.gz
 tar -xzvf chrony-4.3.tar.gz 
 mv chrony-4.3 chrony-4.3-src
@@ -12,6 +13,6 @@ make
 sudo make install
 
 
-sh testnet/tls-gen-cert.sh
+sh /home/ubuntu/scion-time/testnet/tls-gen-cert.sh
 sudo systemctl stop chronyd
 sudo /home/ubuntu/chrony-4.3/sbin/chronyd -4 -f /home/ubuntu/scion-time/testnet/aws/chronyNTS.conf
