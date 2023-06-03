81956007b6ac2f93b3f400934b4ed07760494aa2
Marc Frei <marc.frei@gmail.com>
2023-06-03 14:52:01 +0200

    Add UTC timestamp to ntimed-tool

--- testnet/ntimed/ntimed-tool.c
+++ testnet/ntimed/ntimed-tool.c
@@ -678,11 +678,18 @@ int main(int argc, char *argv[]) {
 			continue;
 		}
 
-		printf("%+.9lf, %+.9lf\n",
+		time_t t2sec = t2.sec;
+		struct tm *gmt = gmtime(&t2sec);
+		assert(gmt != NULL);
+
+		printf("%04d-%02d-%02d'T'%02d:%02d:%02d'Z':%+.9lf, %+.9lf\n",
+			1900 + gmt->tm_year, 1 + gmt->tm_mon, gmt->tm_mday,
+			gmt->tm_hour, gmt->tm_min, gmt->tm_sec,
 			((TS_Diff(&rx_pkt->ntp_receive, &rx_pkt->ntp_origin)
 			+ TS_Diff(&rx_pkt->ntp_transmit, &rx_pkt->ts_rx)) / 2.0),
 			(TS_Diff(&rx_pkt->ts_rx, &rx_pkt->ntp_origin)
 			- TS_Diff(&rx_pkt->ntp_transmit, &rx_pkt->ntp_receive)));
+
 		break;
 	}
 
