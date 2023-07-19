924d1c6985b2c21d6814978daeeb531ffd034025
Julian <11920817+XYQuadrat@users.noreply.github.com>
2023-07-19 09:55:04 +0200

    Add variant in Theil-Sen estimator to drop samples when needed (#18)
    * Add logic to drop old samples if prediction is bad
    
    * Allow enabling/disabling dropping
    
    * Rename bias to err

--- core/sync/theil_sen.go
+++ core/sync/theil_sen.go
@@ -1,6 +1,7 @@
 package sync
 
 import (
+	"math"
 	"sort"
 	"time"
 
@@ -18,6 +19,8 @@ type theilSen struct {
 
 // If the buffer size is too large, the system is likely to oscillate heavily.
 const maxSamples = 4
+const maxAllowedErr = 1000.0
+const dropEnabled = true
 
 const baseFreqGainFactor = 0.005
 
@@ -107,6 +110,33 @@ func (ts *theilSen) AddSample(offset time.Duration) {
 	ts.samples = append(ts.samples, sample{x: now, y: now.Add(offset)})
 }
 
+func runIterations(pts []point) (float64, float64, int) {
+	if !dropEnabled {
+		slope := slope(pts)
+		return slope, intercept(slope, pts), 0
+	}
+
+	n := len(pts)
+
+	for i := 0; i < n; i++ {
+		slope := slope(pts[i:])
+		intercept := intercept(slope, pts[i:])
+
+		err := 0.0
+		for j := i; j < n; j++ {
+			err += math.Abs(prediction(slope, intercept, float64(pts[j].x)) - float64(pts[j].y))
+		}
+		err /= float64(n - i)
+
+		if math.Abs(err) < maxAllowedErr {
+			return slope, intercept, i
+		}
+	}
+
+	defaultSlope := slope(pts)
+	return defaultSlope, intercept(defaultSlope, pts), 0
+}
+
 func (ts *theilSen) Offset() (time.Duration, bool) {
 	if len(ts.samples) == 0 {
 		return time.Duration(0), false
@@ -114,13 +144,16 @@ func (ts *theilSen) Offset() (time.Duration, bool) {
 
 	now := ts.clk.Now()
 	regressionPts := regressionPts(ts.samples)
-	slope := slope(regressionPts)
-	intercept := intercept(slope, regressionPts)
+	slope, intercept, bestStart := runIterations(regressionPts)
+
 	predictedTime := prediction(slope, intercept, float64(now.Sub(ts.samples[0].x).Nanoseconds()))
 	predictedOffset := predictedTime - float64(now.Sub(ts.samples[0].x).Nanoseconds())
 
+	ts.samples = ts.samples[bestStart:]
+
 	ts.log.Debug("Theil-Sen estimate",
 		zap.Int("# of samples", len(ts.samples)),
+		zap.Int("best start", bestStart),
 		zap.Float64("slope", slope),
 		zap.Float64("intercept", intercept),
 		zap.Float64("predicted offset (ns)", predictedOffset),
