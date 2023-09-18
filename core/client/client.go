dda530886bd26ef331528f4315d49a44de4eb4c4
Marc Frei <marc.frei@gmail.com>
2023-09-18 16:05:22 +0200

    Perform three measurements in interleaved mode

--- core/client/client.go
+++ core/client/client.go
@@ -52,7 +52,7 @@ func MeasureClockOffsetIP(ctx context.Context, log *zap.Logger,
 	var off time.Duration
 	var nerr, n int
 	if ntpc.InterleavedMode {
-		n = 2
+		n = 3
 	} else {
 		n = 1
 	}
@@ -131,7 +131,7 @@ func MeasureClockOffsetSCION(ctx context.Context, log *zap.Logger,
 			)
 			if ntpc.InterleavedMode {
 				ntpc.ResetInterleavedMode()
-				n = 2
+				n = 3
 			} else {
 				n = 1
 			}
