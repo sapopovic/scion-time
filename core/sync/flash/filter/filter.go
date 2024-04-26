a2bc1a6d4eb52792f61115966882e8e47075c710
Marc Frei <marc.frei@gmail.com>
2024-04-26 11:51:05 +0200

    Start work on lucky packet filter

--- core/sync/flash/filter/filter.go
+++ core/sync/flash/filter/filter.go
@@ -1,16 +1,48 @@
 package filter
 
-import "time"
+import (
+	"time"
 
-const (
-	DefaultFilterSize = 16
-	DefaultFilterPick = 1
+	"example.com/scion-time/net/ntp"
+
+	"example.com/scion-time/core/measurement"
 )
 
-//lint:ignore U1000 WIP
-type measurement struct {
-	cTxTime time.Time
-	sRxTime time.Time
-	sTxTime time.Time
-	cRxTime time.Time
+const (
+	defaultFilterSize = 16
+)
+
+type filterItem struct {
+	off time.Duration
+	rtd time.Duration
+}
+
+type filter struct {
+	reference string
+	state     []filterItem
+}
+
+func NewLuckyPacketFilter() measurement.Filter {
+	return &filter{}
+}
+
+func (f *filter) Do(reference string, cTxTime, sRxTime, sTxTime, cRxTime time.Time) (
+	offset time.Duration) {
+	if reference == "" {
+		panic("invalid argument: reference must not be \"\"")
+	}
+	if reference != f.reference {
+		if f.reference != "" {
+			panic("filter must be used with a single reference")
+		}
+		f.reference = reference
+	}
+	if len(f.state) == defaultFilterSize {
+		f.state = f.state[1:]
+	}
+	f.state = append(f.state, filterItem{
+		off: ntp.ClockOffset(cTxTime, sRxTime, sTxTime, cRxTime),
+		rtd: ntp.RoundTripDelay(cTxTime, sRxTime, sTxTime, cRxTime),
+	})
+	return f.state[len(f.state)-1].off
 }
