350801d5335ed605db7365b28a4ee3ac88be3f8f
Marc Frei <marc.frei@gmail.com>
2024-04-29 17:22:49 +0200

    Simplify lucky packet filter tests

--- core/sync/flash/filters/filter_test.go
+++ core/sync/flash/filters/filter_test.go
@@ -13,7 +13,10 @@ type measurement struct {
 	cTxTime, sRxTime, sTxTime, cRxTime time.Time
 }
 
-var t0 time.Time
+func at(d int64) time.Time {
+	var t0 time.Time
+	return t0.Add(time.Duration(d) * time.Millisecond)
+}
 
 func offset(m measurement) time.Duration {
 	return ntp.ClockOffset(m.cTxTime, m.sRxTime, m.sTxTime, m.cRxTime)
@@ -24,45 +27,27 @@ func doFilter(f *filters.LuckyPacketFilter, m measurement) time.Duration {
 }
 
 func TestFilter0(t *testing.T) {
-	f := filters.NewLuckyPacketFilter(3 /* cap */, 1 /* pick */)
-	x := measurement{
-		cTxTime: t0,
-		sRxTime: t0.Add(10 * time.Millisecond),
-		sTxTime: t0.Add(11 * time.Millisecond),
-		cRxTime: t0.Add(21 * time.Millisecond),
-	}
-	off0 := doFilter(f, x)
-	off1 := offset(x)
+	f := &filters.LuckyPacketFilter{}
+	x := measurement{cTxTime: at(0), sRxTime: at(10), sTxTime: at(10), cRxTime: at(20)}
+	off0, off1 := doFilter(f, x), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
 }
 
 func TestFilter1(t *testing.T) {
-	f := filters.NewLuckyPacketFilter(3 /* cap */, 1 /* pick */)
-	x := measurement{
-		cTxTime: t0,
-		sRxTime: t0.Add(9 * time.Millisecond),
-		sTxTime: t0.Add(10 * time.Millisecond),
-		cRxTime: t0.Add(21 * time.Millisecond),
-	}
-	off0 := doFilter(f, x)
-	off1 := offset(x)
+	f := &filters.LuckyPacketFilter{}
+	x := measurement{cTxTime: at(0), sRxTime: at(9), sTxTime: at(9), cRxTime: at(20)}
+	off0, off1 := doFilter(f, x), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
 }
 
 func TestFilter2(t *testing.T) {
-	f := filters.NewLuckyPacketFilter(3 /* cap */, 1 /* pick */)
-	x := measurement{
-		cTxTime: t0,
-		sRxTime: t0.Add(11 * time.Millisecond),
-		sTxTime: t0.Add(12 * time.Millisecond),
-		cRxTime: t0.Add(21 * time.Millisecond),
-	}
-	off0 := doFilter(f, x)
-	off1 := offset(x)
+	f := &filters.LuckyPacketFilter{}
+	x := measurement{cTxTime: at(0), sRxTime: at(11), sTxTime: at(11), cRxTime: at(20)}
+	off0, off1 := doFilter(f, x), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
@@ -70,36 +55,22 @@ func TestFilter2(t *testing.T) {
 
 func TestFilter3(t *testing.T) {
 	f := filters.NewLuckyPacketFilter(3 /* cap */, 1 /* pick */)
-	x := measurement{
-		cTxTime: t0,
-		sRxTime: t0.Add(10 * time.Millisecond),
-		sTxTime: t0.Add(11 * time.Millisecond),
-		cRxTime: t0.Add(21 * time.Millisecond),
-	}
-	y := measurement{
-		cTxTime: t0,
-		sRxTime: t0.Add(19 * time.Millisecond),
-		sTxTime: t0.Add(20 * time.Millisecond),
-		cRxTime: t0.Add(41 * time.Millisecond),
-	}
+	x := measurement{cTxTime: at(0), sRxTime: at(10), sTxTime: at(10), cRxTime: at(20)}
+	y := measurement{cTxTime: at(0), sRxTime: at(19), sTxTime: at(19), cRxTime: at(40)}
 	var off0, off1 time.Duration
-	off0 = doFilter(f, x)
-	off1 = offset(x)
+	off0, off1 = doFilter(f, x), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
-	off0 = doFilter(f, y)
-	off1 = offset(x)
+	off0, off1 = doFilter(f, y), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
-	off0 = doFilter(f, y)
-	off1 = offset(x)
+	off0, off1 = doFilter(f, y), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
-	off0 = doFilter(f, y)
-	off1 = offset(y)
+	off0, off1 = doFilter(f, y), offset(y)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
