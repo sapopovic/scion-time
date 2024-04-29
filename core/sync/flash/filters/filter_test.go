22cc0890d14dc73e2da9c9757c9801acc812ee1a
Marc Frei <marc.frei@gmail.com>
2024-04-29 17:29:53 +0200

    Simplify lucky packet filter tests

--- core/sync/flash/filters/filter_test.go
+++ core/sync/flash/filters/filter_test.go
@@ -22,14 +22,14 @@ func offset(m measurement) time.Duration {
 	return ntp.ClockOffset(m.cTxTime, m.sRxTime, m.sTxTime, m.cRxTime)
 }
 
-func doFilter(f *filters.LuckyPacketFilter, m measurement) time.Duration {
+func filter(f *filters.LuckyPacketFilter, m measurement) time.Duration {
 	return f.Do("test", m.cTxTime, m.sRxTime, m.sTxTime, m.cRxTime)
 }
 
 func TestFilter0(t *testing.T) {
 	f := &filters.LuckyPacketFilter{}
 	x := measurement{cTxTime: at(0), sRxTime: at(10), sTxTime: at(10), cRxTime: at(20)}
-	off0, off1 := doFilter(f, x), offset(x)
+	off0, off1 := filter(f, x), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
@@ -38,7 +38,7 @@ func TestFilter0(t *testing.T) {
 func TestFilter1(t *testing.T) {
 	f := &filters.LuckyPacketFilter{}
 	x := measurement{cTxTime: at(0), sRxTime: at(9), sTxTime: at(9), cRxTime: at(20)}
-	off0, off1 := doFilter(f, x), offset(x)
+	off0, off1 := filter(f, x), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
@@ -47,7 +47,7 @@ func TestFilter1(t *testing.T) {
 func TestFilter2(t *testing.T) {
 	f := &filters.LuckyPacketFilter{}
 	x := measurement{cTxTime: at(0), sRxTime: at(11), sTxTime: at(11), cRxTime: at(20)}
-	off0, off1 := doFilter(f, x), offset(x)
+	off0, off1 := filter(f, x), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
@@ -58,21 +58,20 @@ func TestFilter3(t *testing.T) {
 	x := measurement{cTxTime: at(0), sRxTime: at(10), sTxTime: at(10), cRxTime: at(20)}
 	y := measurement{cTxTime: at(0), sRxTime: at(19), sTxTime: at(19), cRxTime: at(40)}
 	var off0, off1 time.Duration
-	off0, off1 = doFilter(f, x), offset(x)
+	off0, off1 = filter(f, x), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
-	off0, off1 = doFilter(f, y), offset(x)
+	off0, off1 = filter(f, y), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
-	off0, off1 = doFilter(f, y), offset(x)
+	off0, off1 = filter(f, y), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
-	off0, off1 = doFilter(f, y), offset(y)
+	off0, off1 = filter(f, y), offset(y)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
 }
-
