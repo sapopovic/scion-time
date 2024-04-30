1073ec6a4e3cc428da841cde523b0fce4ded1d37
Marc Frei <marc.frei@gmail.com>
2024-04-30 21:32:36 +0200

    Add more lucky packet filter tests

--- core/sync/flash/filters/filter_test.go
+++ core/sync/flash/filters/filter_test.go
@@ -55,22 +55,72 @@ func TestFilter2(t *testing.T) {
 
 func TestFilter3(t *testing.T) {
 	f := filters.NewLuckyPacketFilter(3 /* cap */, 1 /* pick */)
+	a := measurement{cTxTime: at(0), sRxTime: at(19), sTxTime: at(19), cRxTime: at(40)}
 	x := measurement{cTxTime: at(0), sRxTime: at(10), sTxTime: at(10), cRxTime: at(20)}
-	y := measurement{cTxTime: at(0), sRxTime: at(19), sTxTime: at(19), cRxTime: at(40)}
 	var off0, off1 time.Duration
 	off0, off1 = filter(f, x), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
-	off0, off1 = filter(f, y), offset(x)
+	off0, off1 = filter(f, a), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
-	off0, off1 = filter(f, y), offset(x)
+	off0, off1 = filter(f, a), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
-	off0, off1 = filter(f, y), offset(y)
+	off0, off1 = filter(f, a), offset(a)
+	if off0 != off1 {
+		t.Errorf("got %q, want %q", off0, off1)
+	}
+}
+
+func TestFilter4(t *testing.T) {
+	f := filters.NewLuckyPacketFilter(5 /* cap */, 1 /* pick */)
+	a := measurement{cTxTime: at(0), sRxTime: at(19), sTxTime: at(19), cRxTime: at(40)}
+	x := measurement{cTxTime: at(0), sRxTime: at(10), sTxTime: at(10), cRxTime: at(20)}
+	_ = filter(f, a)
+	_ = filter(f, a)
+	_ = filter(f, x)
+	_ = filter(f, a)
+	_ = filter(f, a)
+	var off0, off1 time.Duration
+	off0, off1 = filter(f, a), offset(x)
+	if off0 != off1 {
+		t.Errorf("got %q, want %q", off0, off1)
+	}
+}
+
+func TestFilter5(t *testing.T) {
+	f := filters.NewLuckyPacketFilter(5 /* cap */, 5 /* pick */)
+	a := measurement{cTxTime: at(0), sRxTime: at(19), sTxTime: at(19), cRxTime: at(40)}
+	b := measurement{cTxTime: at(0), sRxTime: at(21), sTxTime: at(21), cRxTime: at(40)}
+	x := measurement{cTxTime: at(0), sRxTime: at(10), sTxTime: at(10), cRxTime: at(20)}
+	_ = filter(f, a)
+	_ = filter(f, a)
+	_ = filter(f, b)
+	_ = filter(f, b)
+	var off0, off1 time.Duration
+	off0, off1 = filter(f, x), offset(x)
+	if off0 != off1 {
+		t.Errorf("got %q, want %q", off0, off1)
+	}
+}
+
+func TestFilter6(t *testing.T) {
+	f := filters.NewLuckyPacketFilter(5 /* cap */, 3 /* pick */)
+	a := measurement{cTxTime: at(0), sRxTime: at(29), sTxTime: at(29), cRxTime: at(60)}
+	b := measurement{cTxTime: at(0), sRxTime: at(19), sTxTime: at(19), cRxTime: at(40)}
+	c := measurement{cTxTime: at(0), sRxTime: at(21), sTxTime: at(21), cRxTime: at(40)}
+	d := measurement{cTxTime: at(0), sRxTime: at(31), sTxTime: at(31), cRxTime: at(60)}
+	x := measurement{cTxTime: at(0), sRxTime: at(10), sTxTime: at(10), cRxTime: at(20)}
+	_ = filter(f, a)
+	_ = filter(f, b)
+	_ = filter(f, c)
+	_ = filter(f, d)
+	var off0, off1 time.Duration
+	off0, off1 = filter(f, x), offset(x)
 	if off0 != off1 {
 		t.Errorf("got %q, want %q", off0, off1)
 	}
