1075d152ee5619e6901f42496dc57cb0f3541dcb
Marc Frei <marc.frei@gmail.com>
2024-05-05 10:59:26 +0200

    Read in mbgsvcd data and tokenize each line

--- testnet/mbgplot/mbgplot.go
+++ testnet/mbgplot/mbgplot.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("mbgplot")
+	var fn string
+	flag.StringVar(&fn, "f", "", "Mbg data file to read")
+	flag.Parse()
+	f, err := os.Open(fn)
+	if err != nil {
+		log.Fatalf("failed to open file: %s", err)
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		l := s.Text()
+		ts := strings.Fields(l) // Tokenize the line
+		fmt.Println(ts)
+	}
+	if err := s.Err(); err != nil {
+		log.Fatalf("error during scan: %s", err)
+	}
 }
