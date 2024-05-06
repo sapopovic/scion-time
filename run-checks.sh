06ea6d9efc37d85f1d2eac11ffc6e4ca72bfe2b3
Marc Frei <marc.frei@gmail.com>
2024-05-06 09:44:16 +0200

    Cleanup

--- run-checks.sh
+++ run-checks.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+# To install govulncheck see https://go.dev/doc/security/vuln/
 # To install staticcheck see https://staticcheck.dev/docs/getting-started/#installation
 # To install golangci-lint see https://golangci-lint.run/welcome/install/
-go install golang.org/x/vuln/cmd/govulncheck@latest
 
 env GOOS=linux GOARCH=amd64 go vet ./...
 env GOOS=linux GOARCH=amd64 govulncheck ./...
