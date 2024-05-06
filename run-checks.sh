b72ef89bc94b9d3d339e1b4e3c9abb5458ef39d9
Marc Frei <marc.frei@gmail.com>
2024-05-06 09:21:30 +0200

    Add Go vulnerability detection check

--- run-checks.sh
+++ run-checks.sh
@@ -3,7 +3,9 @@ set -Eeuo pipefail
 
 # To install staticcheck see https://staticcheck.dev/docs/getting-started/#installation
 # To install golangci-lint see https://golangci-lint.run/welcome/install/
+go install golang.org/x/vuln/cmd/govulncheck@latest
 
 env GOOS=linux GOARCH=amd64 go vet ./...
+env GOOS=linux GOARCH=amd64 govulncheck ./...
 env GOOS=linux GOARCH=amd64 staticcheck -checks "all,-ST1000" ./...
 env GOOS=linux GOARCH=amd64 golangci-lint run
