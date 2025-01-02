2da98f0746d78b3ab9dfca00bf2641f59753b099
Marc Frei <marc.frei@gmail.com>
2025-01-02 17:27:44 +0100

    Work on CSPTP message format

--- net/csptp/csptp.go
+++ net/csptp/csptp.go
@@ -1,6 +1,24 @@
 package csptp
 
-type Packet struct {}
+const (
+	EventPortIP      = 319   // Sync
+	EventPortSCION   = 10319 // Sync
+	GeneralPortIP    = 320   // Follow Up
+	GeneralPortSCION = 10320 // Follow Up
+
+	SdoID = 0
+
+	MessageTypeSync     = 0
+	MessageTypeFollowUp = 8
+
+	VersionMin     = 1
+	VersionMax     = 0x12
+	VersionDefault = 0x12
+
+	DomainNumber = 0
+)
+
+type Packet struct{}
 
 func DecodePacket(pkt *Packet, b []byte) error {
 	return nil
