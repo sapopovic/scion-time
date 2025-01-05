ecd8bdc3c30ede5e8d7da105a06b8be2748276e0
Marc Frei <marc.frei@gmail.com>
2025-01-05 11:41:10 +0100

    Work on CSPTP message format

--- net/csptp/csptp.go
+++ net/csptp/csptp.go
@@ -8,17 +8,40 @@ const (
 
 	SdoID = 0
 
-	MessageTypeSync     = 0
-	MessageTypeFollowUp = 8
+	MsgTypeSync     = 0
+	MsgTypeFollowUp = 8
 
 	VersionMin     = 1
 	VersionMax     = 0x12
 	VersionDefault = 0x12
 
-	DomainNumber = 0
+	Domain = 0
 )
 
-type Packet struct{}
+type PortID struct {
+	ClockID uint64
+	Port    uint16
+}
+
+type Timestamp struct {
+	Seconds     [6]uint8
+	Nanoseconds uint32
+}
+
+type Packet struct {
+	SdoIDMsgType    uint8
+	Version         uint8
+	MsgLen          uint16
+	Domain          uint8
+	Flags           uint16
+	Correction      int64
+	MsgTypeSpecific uint32
+	PortID          PortID
+	SeqID           uint16
+	MsgCtrl         uint8
+	LogMsgPeriod    int8
+	Timestamp       Timestamp
+}
 
 func DecodePacket(pkt *Packet, b []byte) error {
 	return nil
