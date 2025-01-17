b8a41449b22478029563590b83315fa8c0fbfb18
Marc Frei <marc.frei@gmail.com>
2025-01-17 10:49:43 +0100

    Clean up CSPTP interface

--- net/csptp/csptp.go
+++ net/csptp/csptp.go
@@ -18,7 +18,7 @@ const (
 	MessageTypeSync     = 0
 	MessageTypeFollowUp = 8
 
-	DefaultVersion = 0x12
+	PTPVersion = 0x12
 
 	DomainNumber = 0
 
@@ -27,9 +27,9 @@ const (
 	FlagCurrentUTCOffsetValid = 1 << 2
 	FlagPTPTimescale          = 1 << 3
 
-	MessageControlSync     = 0
-	MessageControlFollowUp = 2
-	MessageControlOther    = 5
+	ControlSync     = 0
+	ControlFollowUp = 2
+	ControlOther    = 5
 
 	LogMessageInterval = 0x7f
 
@@ -63,19 +63,19 @@ type Timestamp struct {
 }
 
 type Message struct {
-	SdoIDType          uint8
-	Version            uint8
-	Length             uint16
-	DomainNumber       uint8
-	MinorSdoID         uint8
-	Flags              uint16
-	CorrectionField    int64
-	TypeSpecific       uint32
-	SourcePortIdentity PortID
-	SequenceID         uint16
-	ControlField       uint8
-	LogMessageInterval int8
-	Timestamp          Timestamp
+	SdoIDMessageType    uint8
+	PTPVersion          uint8
+	MessageLength       uint16
+	DomainNumber        uint8
+	MinorSdoID          uint8
+	FlagField           uint16
+	CorrectionField     int64
+	MessageTypeSpecific uint32
+	SourcePortIdentity  PortID
+	SequenceID          uint16
+	ControlField        uint8
+	LogMessageInterval  int8
+	Timestamp           Timestamp
 }
 
 type RequestTLV struct {
@@ -83,7 +83,7 @@ type RequestTLV struct {
 	Length              uint16
 	OrganizationID      [3]uint8
 	OrganizationSubType [3]uint8
-	Flags               uint32
+	FlagField           uint32
 }
 
 type ServerStateDS struct {
@@ -103,7 +103,7 @@ type ResponseTLV struct {
 	Length                  uint16
 	OrganizationID          [3]uint8
 	OrganizationSubType     [3]uint8
-	Flags                   uint32
+	FlagField               uint32
 	Error                   uint16
 	RequestIngressTimestamp Timestamp
 	RequestCorrectionField  int64
