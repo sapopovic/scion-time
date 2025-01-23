df8e88e0f26023e5e2b2e429852fefecfcef9900
Marc Frei <marc.frei@gmail.com>
2025-01-23 17:59:03 +0100

    Implement sending of CSPTP Sync and Follow Up messages

--- core/client/client_csptp_ip.go
+++ core/client/client_csptp_ip.go
@@ -22,7 +22,7 @@ type CSPTPClientIP struct {
 func (c *CSPTPClientIP) MeasureClockOffset(ctx context.Context, localAddr, remoteAddr netip.Addr) (
 	timestamp time.Time, offset time.Duration, err error) {
 	var lc net.ListenConfig
-	pconn, err := lc.ListenPacket(ctx, "udb", netip.AddrPortFrom(localAddr, 0).String())
+	pconn, err := lc.ListenPacket(ctx, "udp", netip.AddrPortFrom(localAddr, 0).String())
 	if err != nil {
 		return time.Time{}, 0, err
 	}
@@ -45,10 +45,101 @@ func (c *CSPTPClientIP) MeasureClockOffset(ctx context.Context, localAddr, remot
 	}
 
 	buf := make([]byte, csptp.MaxMessageLength)
+	var n int
+	var msg csptp.Message
+	var reqtlv csptp.RequestTLV
 
 	cTxTime0 := timebase.Now()
 
-	_, _ = buf, cTxTime0
+	msg = csptp.Message{
+		SdoIDMessageType:    csptp.MessageTypeSync,
+		PTPVersion:          csptp.PTPVersion,
+		MessageLength:       csptp.MinMessageLength,
+		DomainNumber:        csptp.DomainNumber,
+		MinorSdoID:          csptp.MinorSdoID,
+		FlagField:           csptp.FlagTwoStep | csptp.FlagUnicast,
+		CorrectionField:     0,
+		MessageTypeSpecific: 0,
+		SourcePortIdentity: csptp.PortID{
+			ClockID: 0,
+			Port:    1,
+		},
+		SequenceID:         0,
+		ControlField:       csptp.ControlSync,
+		LogMessageInterval: 0,
+		Timestamp:          csptp.Timestamp{},
+	}
+
+	buf = buf[:msg.MessageLength]
+	csptp.EncodeMessage(buf, &msg)
+
+	n, err = conn.WriteToUDPAddrPort(buf, netip.AddrPortFrom(remoteAddr, csptp.EventPortIP))
+	if err != nil {
+		return time.Time{}, 0, err
+	}
+	if n != len(buf) {
+		return time.Time{}, 0, errWrite
+	}
+	cTxTime1, id, err := udp.ReadTXTimestamp(conn)
+	if err != nil || id != 0 {
+		cTxTime1 = timebase.Now()
+		c.Log.LogAttrs(ctx, slog.LevelError, "failed to read packet tx timestamp", slog.Any("error", err))
+	}
+
+	buf = buf[:cap(buf)]
+
+	msg = csptp.Message{
+		SdoIDMessageType:    csptp.MessageTypeFollowUp,
+		PTPVersion:          csptp.PTPVersion,
+		MessageLength:       csptp.MinMessageLength,
+		DomainNumber:        csptp.DomainNumber,
+		MinorSdoID:          csptp.MinorSdoID,
+		FlagField:           csptp.FlagUnicast,
+		CorrectionField:     0,
+		MessageTypeSpecific: 0,
+		SourcePortIdentity: csptp.PortID{
+			ClockID: 0,
+			Port:    1,
+		},
+		SequenceID:         0,
+		ControlField:       csptp.ControlFollowUp,
+		LogMessageInterval: 0,
+		Timestamp:          csptp.Timestamp{},
+	}
+	reqtlv = csptp.RequestTLV{
+		Type:   csptp.TLVTypeOrganizationExtension,
+		Length: 0,
+		OrganizationID: [3]uint8{
+			csptp.OrganizationIDMeinberg0,
+			csptp.OrganizationIDMeinberg1,
+			csptp.OrganizationIDMeinberg2},
+		OrganizationSubType: [3]uint8{
+			csptp.OrganizationSubTypeRequest0,
+			csptp.OrganizationSubTypeRequest1,
+			csptp.OrganizationSubTypeRequest2},
+		FlagField: csptp.TLVFlagServerStateDS,
+	}
+	msg.MessageLength += uint16(csptp.EncodedRequestTLVLength(&reqtlv))
+	reqtlv.Length = uint16(csptp.EncodedRequestTLVLength(&reqtlv))
+
+	buf = buf[:msg.MessageLength]
+	csptp.EncodeMessage(buf[:csptp.MinMessageLength], &msg)
+	csptp.EncodeRequestTLV(buf[csptp.MinMessageLength:], &reqtlv)
+
+	n, err = conn.WriteToUDPAddrPort(buf, netip.AddrPortFrom(remoteAddr, csptp.GeneralPortIP))
+	if err != nil {
+		return time.Time{}, 0, err
+	}
+	if n != len(buf) {
+		return time.Time{}, 0, errWrite
+	}
+	cTxTime2, id, err := udp.ReadTXTimestamp(conn)
+	if err != nil || id != 0 {
+		cTxTime1 = timebase.Now()
+		c.Log.LogAttrs(ctx, slog.LevelError, "failed to read packet tx timestamp", slog.Any("error", err))
+	}
+
+	_, _, _ = cTxTime0, cTxTime1, cTxTime2
 
 	return
 }
