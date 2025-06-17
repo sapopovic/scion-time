fdb8ed262847432933a3fea4f9bfe2cb14415fe6
Sanja <sapopovic99@gmail.com>
2025-06-17 20:31:50 +0000

    Move path selection logic and write code for pinging

---
+++ core/client/path_manager.go
@@ -0,0 +1,507 @@
+package client
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"log/slog"
+	"math"
+	"math/big"
+	"net"
+	"net/netip"
+	"time"
+
+	"example.com/scion-time/core/timebase"
+	"example.com/scion-time/net/udp"
+	"github.com/google/gopacket"
+	"github.com/scionproto/scion/pkg/addr"
+	"github.com/scionproto/scion/pkg/slayers"
+	"github.com/scionproto/scion/pkg/snet"
+)
+
+func ChooseNewPaths(availablePaths []snet.Path, numPaths int) []snet.Path {
+	ch := make(chan int, 1)
+	timeout, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var computedPathSet []snet.Path
+
+	go func() {
+		computedPathSet = greedyDisjointPathSelection(availablePaths, len(availablePaths), numPaths)
+		ch <- 1
+	}()
+
+	log := slog.Default()
+	select {
+	case <-timeout.Done():
+		log.Warn(fmt.Sprintf("Path selection took too long! Using first few paths"))
+		return availablePaths[:min(len(availablePaths), numPaths)]
+	case <-ch:
+		return computedPathSet
+	}
+}
+
+func greedyDisjointPathSelection(paths []snet.Path, nbOfPaths int, k int) []snet.Path {
+	if len(paths) <= k {
+		return paths
+	}
+
+	selected := []snet.Path{}
+	usedInterfaces := map[snet.PathInterface]int{}
+
+	// Step 1: Start with a RANDOM path
+	startIdx := secureRandomIndex(len(paths)) // Starting with a random path
+	best := paths[startIdx]
+	selected = append(selected, best)
+	for _, iface := range best.Metadata().Interfaces {
+		usedInterfaces[iface]++
+	}
+
+	// Step 2: Greedily add most disjoint paths
+	for len(selected) < k {
+		var nextBest snet.Path
+		bestScore := math.MinInt
+		bestHopCount := math.MaxInt
+		for _, candidate := range paths {
+			if alreadySelected(candidate, selected) {
+				continue
+			}
+
+			score := disjointnessScore(candidate, usedInterfaces)
+			hopCount := len(candidate.Metadata().Interfaces)
+			if score > bestScore || (score == bestScore && hopCount < bestHopCount) { // second part is about prefering paths that are shorter
+				nextBest = candidate
+				bestScore = score
+				bestHopCount = hopCount
+			}
+		}
+
+		if nextBest != nil {
+			selected = append(selected, nextBest)
+			for _, iface := range nextBest.Metadata().Interfaces {
+				usedInterfaces[iface]++
+			}
+		} else {
+			break
+		}
+
+	}
+
+	// Step 3: Fill up to k paths using remaining unused paths
+	/*for len(selected) < k {
+		var nextBest snet.Path
+		bestScore := math.MinInt
+		for _, candidate := range paths {
+			if alreadySelected(candidate, selected) {
+				continue
+			}
+			score := disjointnessScore(candidate, usedInterfaces)
+			if score > bestScore {
+				bestScore = score
+				nextBest = candidate
+			}
+		}
+		if nextBest != nil {
+			selected = append(selected, nextBest)
+			for _, iface := range nextBest.Metadata().Interfaces {
+				usedInterfaces[iface]++
+			}
+		} else {
+			break // nothing left
+		}
+	}*/
+
+	return selected
+}
+
+func secureRandomIndex(n int) int {
+	if n <= 0 {
+		return 0
+	}
+	max := big.NewInt(int64(n))
+	i, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		return 0
+	}
+	return int(i.Int64())
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func alreadySelected(p snet.Path, selected []snet.Path) bool {
+	for _, s := range selected {
+		if snet.Fingerprint(s) == snet.Fingerprint(p) {
+			return true
+		}
+	}
+	return false
+}
+
+func disjointnessScore(p snet.Path, used map[snet.PathInterface]int) int {
+	score := 0
+	for _, iface := range p.Metadata().Interfaces {
+		score -= used[iface]
+	}
+	// Zero score = fully disjoint.
+	// Negative score = many shared interfaces.
+	return score
+}
+
+// -------------------network----------------------------
+
+func Ping(ctx context.Context, localAddr, remoteAddr udp.UDPAddr, path snet.Path) (rtt time.Duration, err error) {
+
+	// --------------------sending-----------------------
+	log := slog.Default()
+	laddr, ok := netip.AddrFromSlice(localAddr.Host.IP)
+	if !ok {
+		return 0, nil
+	}
+	var lc net.ListenConfig
+	pconn, err := lc.ListenPacket(ctx, "udp", netip.AddrPortFrom(laddr, 0).String())
+	if err != nil {
+		return 0, nil
+	}
+	conn := pconn.(*net.UDPConn)
+	defer func() { _ = conn.Close() }()
+	deadline, deadlineIsSet := ctx.Deadline()
+	if deadlineIsSet {
+		err = conn.SetDeadline(deadline)
+		if err != nil {
+			return 0, err
+		}
+	}
+	err = udp.EnableTimestamping(conn, localAddr.Host.Zone)
+	if err != nil {
+		log.LogAttrs(ctx, slog.LevelError, "failed to enable timestamping", slog.Any("error", err))
+	}
+	// err = udp.SetDSCP(conn, c.DSCP)
+	// if err != nil {
+	// 	c.Log.LogAttrs(ctx, slog.LevelInfo, "failed to set DSCP", slog.Any("error", err))
+	// }
+
+	localPort := conn.LocalAddr().(*net.UDPAddr).Port
+
+	ip4 := remoteAddr.Host.IP.To4()
+	if ip4 != nil {
+		remoteAddr.Host.IP = ip4
+	}
+
+	nextHop := path.UnderlayNextHop().AddrPort()
+	nextHopAddr := nextHop.Addr()
+	if nextHopAddr.Is4In6() {
+		nextHop = netip.AddrPortFrom(
+			netip.AddrFrom4(nextHopAddr.As4()),
+			nextHop.Port())
+	}
+
+	buf := gopacket.Payload([]byte("ping"))
+
+	var scionLayer slayers.SCION
+	//scionLayer.TrafficClass = c.DSCP << 2
+	scionLayer.SrcIA = localAddr.IA
+	srcAddrIP, ok := netip.AddrFromSlice(localAddr.Host.IP)
+	if !ok {
+		panic(errUnexpectedAddrType)
+	}
+	err = scionLayer.SetSrcAddr(addr.HostIP(srcAddrIP.Unmap()))
+	if err != nil {
+		panic(err)
+	}
+	scionLayer.DstIA = remoteAddr.IA
+	dstAddrIP, ok := netip.AddrFromSlice(remoteAddr.Host.IP)
+	if !ok {
+		panic(errUnexpectedAddrType)
+	}
+	err = scionLayer.SetDstAddr(addr.HostIP(dstAddrIP.Unmap()))
+	if err != nil {
+		panic(err)
+	}
+	err = path.Dataplane().SetPath(&scionLayer)
+	if err != nil {
+		panic(err)
+	}
+	scionLayer.NextHdr = slayers.L4UDP
+
+	var udpLayer slayers.UDP
+	udpLayer.SrcPort = uint16(localPort)
+	udpLayer.DstPort = uint16(remoteAddr.Host.Port)
+	udpLayer.SetNetworkLayerForChecksum(&scionLayer)
+
+	payload := gopacket.Payload(buf)
+
+	buffer := gopacket.NewSerializeBuffer()
+	options := gopacket.SerializeOptions{
+		ComputeChecksums: true,
+		FixLengths:       true,
+	}
+
+	err = payload.SerializeTo(buffer, options)
+	if err != nil {
+		panic(err)
+	}
+	buffer.PushLayer(payload.LayerType())
+
+	err = udpLayer.SerializeTo(buffer, options)
+	if err != nil {
+		panic(err)
+	}
+	buffer.PushLayer(udpLayer.LayerType())
+
+	err = scionLayer.SerializeTo(buffer, options)
+	if err != nil {
+		panic(err)
+	}
+	buffer.PushLayer(scionLayer.LayerType())
+
+	n, err := conn.WriteToUDPAddrPort(buffer.Bytes(), nextHop)
+	if err != nil {
+		return 0, err
+	}
+	if n != len(buffer.Bytes()) {
+		return 0, errWrite
+	}
+	cTxTime1, id, err := udp.ReadTXTimestamp(conn)
+	if err != nil || id != 0 {
+		cTxTime1 = timebase.Now()
+		log.LogAttrs(ctx, slog.LevelError, "failed to read packet tx timestamp", slog.Any("error", err))
+	}
+
+	// --------------------receiving-----------------------
+
+	oob := make([]byte, udp.TimestampLen())
+	buf = buf[:cap(buf)]
+
+	n, oobn, flags, _, err := conn.ReadMsgUDPAddrPort(buf, oob) // _ = from
+	if err != nil {
+		return 0, err
+	}
+	if flags != 0 {
+		return 0, errUnexpectedPacketFlags
+	}
+
+	oob = oob[:oobn]
+	cRxTime, err := udp.TimestampFromOOBData(oob)
+	if err != nil {
+		cRxTime = timebase.Now()
+		log.LogAttrs(ctx, slog.LevelError, "failed to read RX timestamp", slog.Any("error", err))
+	}
+	buf = buf[:n]
+
+	var (
+		hbhLayer    slayers.HopByHopExtnSkipper
+		e2eLayer    slayers.EndToEndExtn
+		scionLayer2 slayers.SCION
+		udpLayer2   slayers.UDP
+	)
+
+	parser := gopacket.NewDecodingLayerParser(
+		slayers.LayerTypeSCION, &scionLayer2, &hbhLayer, &e2eLayer, &udpLayer2,
+	)
+	parser.IgnoreUnsupported = true
+	decoded := make([]gopacket.LayerType, 4)
+
+	if err := parser.DecodeLayers(buf, &decoded); err != nil {
+		return 0, err
+	}
+
+	validSrc := scionLayer.SrcIA == remoteAddr.IA &&
+		compareIPs(scionLayer.RawSrcAddr, remoteAddr.Host.IP) == 0
+	validDst := scionLayer.DstIA == localAddr.IA &&
+		compareIPs(scionLayer.RawDstAddr, localAddr.Host.IP) == 0
+
+	if !validSrc || !validDst {
+		return 0, errUnexpectedPacket
+	}
+
+	rtt = cRxTime.Sub(cTxTime1)
+	return rtt, nil
+}
+
+// -------------------not scalable-----------------------
+
+type PathPickDescriptor struct {
+	ruleIndex int
+	pathIndex int
+}
+
+type PathPicker struct {
+	pathSpec        *[]PathSpec
+	availablePaths  []snet.Path
+	currentPathPick []PathPickDescriptor
+}
+
+type PathInterface struct {
+	ia   addr.IA
+	ifId uint64
+}
+
+func (iface *PathInterface) ID() uint64 {
+	return iface.ifId
+}
+
+func (iface *PathInterface) IA() addr.IA {
+	return iface.ia
+}
+
+type PathSpec []PathInterface
+
+type AppPathSet map[snet.PathFingerprint]snet.Path
+
+func makePathPicker(pathSet []snet.Path, numPaths int) *PathPicker {
+	paths := make([]snet.Path, 0, len(pathSet))
+	for _, path := range pathSet {
+		paths = append(paths, path)
+	}
+	picker := &PathPicker{
+		availablePaths: paths,
+	}
+	picker.reset(numPaths)
+	return picker
+}
+
+func (picker *PathPicker) reset(numPaths int) {
+	descriptor := make([]PathPickDescriptor, numPaths)
+	for i := range descriptor {
+		descriptor[i].ruleIndex = -1
+		descriptor[i].pathIndex = -1
+	}
+	picker.currentPathPick = descriptor
+}
+
+func (picker *PathPicker) disjointnessScore() int {
+	interfaces := map[snet.PathInterface]int{}
+	score := 0
+	for _, pick := range picker.currentPathPick {
+		for _, path := range picker.availablePaths[pick.pathIndex].Metadata().Interfaces {
+			score -= interfaces[path]
+			interfaces[path]++
+		}
+	}
+	return score
+}
+
+func (picker *PathPicker) nextPick() bool {
+	return picker.nextPickIterate(len(picker.currentPathPick) - 1)
+}
+
+func (picker *PathPicker) nextPickIterate(idx int) bool {
+	if idx > 0 && picker.currentPathPick[idx-1].pathIndex == -1 {
+		if !picker.nextPickIterate(idx - 1) {
+			return false
+		}
+	}
+	for true {
+		for pathIdx := picker.currentPathPick[idx].pathIndex + 1; pathIdx < len(picker.availablePaths); pathIdx++ {
+			if !picker.isInUse(pathIdx, idx) && picker.matches(pathIdx, picker.currentPathPick[idx].ruleIndex) {
+				picker.currentPathPick[idx].pathIndex = pathIdx
+				return true
+			}
+		}
+		// overflow
+		if idx > 0 {
+			picker.currentPathPick[idx].pathIndex = -1
+			if !picker.nextPickIterate(idx - 1) {
+				return false
+			}
+		} else {
+			break // cannot overflow, abort
+		}
+	}
+	return false
+}
+
+func (iface *PathInterface) match(pathIface snet.PathInterface) bool {
+	if iface.ifId == 0 {
+		return iface.IA() == pathIface.IA
+	}
+	return iface.ID() == uint64(pathIface.ID) && iface.IA() == pathIface.IA
+}
+
+func (picker *PathPicker) matches(pathIdx, ruleIdx int) bool {
+	return true
+}
+
+func (picker *PathPicker) isInUse(pathIdx, idx int) bool {
+	for i, pick := range picker.currentPathPick {
+		if i > idx {
+			return false
+		}
+		if pick.pathIndex == pathIdx {
+			return true
+		}
+	}
+	return false
+}
+
+func (picker *PathPicker) nextRuleSet() bool {
+	if picker.currentPathPick[0].ruleIndex == -1 {
+		for i := range picker.currentPathPick {
+			picker.currentPathPick[i].ruleIndex = 0
+			picker.currentPathPick[i].pathIndex = -1
+		}
+		return true
+	}
+	return false
+}
+
+func (picker *PathPicker) maxRuleIdx() int {
+	// rule indices are sorted ascending
+	for idx := len(picker.currentPathPick) - 1; idx >= 0; idx++ {
+		if picker.currentPathPick[idx].ruleIndex != -1 {
+			return picker.currentPathPick[idx].ruleIndex
+		}
+	}
+	return -1
+}
+
+func (picker *PathPicker) getPaths() []snet.Path {
+	paths := make([]snet.Path, 0, len(picker.currentPathPick))
+	for _, pick := range picker.currentPathPick {
+		paths = append(paths, picker.availablePaths[pick.pathIndex])
+	}
+	return paths
+}
+
+func ChooseNewPaths_notscalable(availablePaths []snet.Path, numPaths int) []snet.Path {
+	// Because this path selection takes too long when many paths are available
+	// (tens of seconds), we run it with a timeout and fall back to using the
+	// first few paths if it takes too long.
+	ch := make(chan int, 1)
+	timeout, cancel := context.WithTimeout(context.Background(), 55*time.Minute)
+	defer cancel()
+
+	var computedPathSet []snet.Path
+	go func() { // pick paths
+		picker := makePathPicker(availablePaths, numPaths)
+		disjointness := 0 // negative number denoting how many network interfaces are shared among paths (to be maximized)
+		for i := numPaths; i > 0; i-- {
+			picker.reset(i)
+			for picker.nextPick() { // iterate through different choices of paths obeying the rules of the current set of PathSpecs
+				curDisjointness := picker.disjointnessScore()
+				if computedPathSet == nil || disjointness < curDisjointness { // maximize disjointness
+					disjointness = curDisjointness
+					computedPathSet = picker.getPaths()
+				}
+			}
+			if computedPathSet != nil { // if no path set of size i found, try with i-1
+				break
+			}
+		}
+		ch <- 1
+	}()
+
+	log := slog.Default()
+	select {
+	case <-timeout.Done():
+		log.Warn(fmt.Sprintf("Path selection took too long! Using first few paths"))
+		return availablePaths[:min(len(availablePaths), numPaths)]
+	case <-ch:
+		return computedPathSet
+	}
+}
